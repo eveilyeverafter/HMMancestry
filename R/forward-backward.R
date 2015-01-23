3b7eb8e67cd0f2ed68b88e8bce30c6ecafe054d9
Tyler Hether <tyler.hether@gmail.com>
2015-01-23 11:47:56 -0800

    Fixed an error in the backward prob calculation. Added description of major fb function.

--- R/forward-backward.R
+++ R/forward-backward.R
@@ -22,29 +22,116 @@
 #' 
 #' @param p.trans the per snp (check that) recombination (i.e., transition) probability. 
 #' 
-#' @details \code{tetrad_estimate_anc_fwd_back} attempts to estimate parental genotypic 
-#' 'states' along a chromosome given empirical or simulated F2 cross data. Next-generation 
-#' data inherits both sequencing error and missing data -- especially when sequencing 
-#' coverage is low. In these two cases (sequence error and missing data) the parental state 
-#' is ambiguous or unknown, respectively. \code{tetrad_estimate_anc_fwd_back} takes these
-#' uncertainities into account. 
+#' @details \code{tetrad_estimate_anc_fwd_back} attempts to estimate 
+#' parental genotypic 'states' along a chromosome given empirical or 
+#' simulated F2 cross data were all four spores of a tetrad are genotyped. 
+#' Next-generation data inherits both sequencing error and missing data 
+#' -- especially when sequencing coverage is low. In these two cases 
+#' (sequence error and missing data) the parental state is ambiguous 
+#' or unknown, respectively. \code{tetrad_estimate_anc_fwd_back} takes 
+#' these uncertainties into account.
 #' 
-#' To do - Add details of the actual algorithm
+#' The three steps taken in \code{tetrad_estimate_anc_fwd_back} are: 
+#' \enumerate{
+#'  \item Calculate forward probabilities for each state (5' to 3')
+#'  \item Calculate backward probabilities for each state (3' to 5')
+#'  \item From these two probabilities, calculate the posterior probability
+#'  that a snp location is of a given parental state.
+#' } 
 #' 
-#' @return to do
+#' The two element vector of forward probabilities for each parental state 
+#' \equ{f_{i}} at each position, i, are calculated as:
+#' \dequ{f_{i} = e_{i}T_{i}f_{i-1}}
 #' 
+#' where \equ{e_{i}} is the emission probabilities for each state (see below), \equ{T_{i}}
+#' is a 2-by-2 matrix describing the transition (recombination) probabilities
+#' between two states, and \equ{f_{i-1}} is the forward probability at the 
+#' previous position along the chromosome (5' of position i). It is assumed 
+#' that each state is equally likely to occur at the first position. 
+#' 
+#' 
+#' The emmission probabilities are calculated independently for 
+#' each snp and depends on the sequence reads assigned to parent "0" 
+#' and parent "1" and \code{p.assign}. The emission probabilities are
+#' calculated using the binomial equation. For example, the emission
+#' probability for parental state "0" at snp position i is: 
+#' 
+#' \dequ{e_{i,0} = {n \choose{k0}} p^{k0} (1-p)^{n-k0}}
+#' 
+#' where n is the sum of the number of reads from both parents and p 
+#' is \code{p.assign}. 
+#' 
+#' Like the emission probabilities, the transition probabilities are also
+#' calculated for each snp position. This accounts for the displacement 
+#' between snps.  For example if the per base recombination rate is 
+#' 0.001 and two snps are 10 base pairs apart, the transition probability of
+#' is 0.01 (i.e., the probability of not recombining would be 0.99).
+#' 
+#' To avoid underflow, we rescale the forward (and backward) probabilities
+#' each iteration to that they sum to unity. 
+#' 
+#' The backward probabilities are calculated similarly but in the 3' to 5' 
+#' direction. It is assumed that the backward probability is equally likely 
+#' in each state. Following Durbin et al (1998) the backward probability at
+#' snp position i is: 
+#' 
+#' \dequ{b_{i} = T_{i}e_{i+1}b_{i+1}}
+#' 
+#' Again, these probabilities are rescale to avoid underflow. 
+#' 
+#' The posterior probability that the state at position i is k, \equ{\pi_{i}=k} 
+#' given the observed sequence read counts for each parent at position i, \equ{x_{i}}
+#' is calculated by:  
+#' 
+#' \dequ{P(\pi_{i} = k | x_{i}) = \frac{f_{i} b{i}}{\sum\limits_{k=0}^{1} f_{i}^{(k)} b{i}^{(k)}}}
+#' 
+#' where the super scripts in the denominator are vector indices. 
+#' 
+#' Finally, we can determine the log likelihood of the whole sequence 
+#' of observations by summing up the log of all the scale factors in the 
+#' forward probability calculation. 
+#' 
+#' @return A list of class \code{forward.backward} containing 4 elements corresponding to each 
+#' of the four tetrads. Each element is itself a list containing:
+#' \describe{
+#'     \item The input snp.data
+#'     \item spore_number an integer specifying the spore number
+#'     \item tetrad.id an integer specifying the tetrad number
+#'     \item chr.name a numeric or character specifying the name of the chromosome
+#'     \item snp.locations a vector specifying the snp locations along the chromosome
+#'     \item p.assign a numeric between 0 and 1 specifying the probability of correct 
+#'     sequence assignment
+#'     \item p.trans a numeric between 0 and 1 specifying the per base pair recombination 
+#'     rate (transition probability)
+#'     \item emissions a matrix of 2 columns by length(snp.locations) rows specifying
+#'     the emission probabilities (see details)
+#'     \item forward a matrix of 2 columns by length(snp.locations) rows giving the scaled
+#'     forward probabilities
+#'     \item backward a matrix of 2 columns by length(snp.locations) rows giving the scaled
+#'     backward probabilities
+#'     \item scale a vector of length snp.locations giving the forward scaling factors
+#'     \item posterior a matrix of 2 columns by length(snp.locations) rows giving the posterior
+#'     probabilities for each parental state
+#'     \item states_inferred a vector of length snp.locations giving the state with the
+#'     highest posterior. In the event of a tie, a state is picked randomly. 
+#'     \item lnL a numeric giving the total likelihood of the data (see details).
+#' }
+#'  
 #' @references Hohenlohe, P.A., S. Bassham, M. Currey, and W.A. Cresko. 2012. Extensive linkage 
 #' disequilibrium and parallel adaptive divergence across threespine stickleback genomes. 
 #' Phil. Trans. R. Soc. B, 395-408, doi: 10.1098/rstb.2011.0245
+#'
+#' Drubin, R. S. Eddy, A. Krogh, and G. Mitchison. 1998. Biological Sequence Analysis: Probabilistic Models
+#' of proteins and nucleic acids. Cambridge University Press, Cambridge CB2 8RU, UK.
 #' 
-#' @seealso \code{\link{simulate_coverage}}
+#' @seealso \code{\link{simulate_coverage}}, \code{\link{est_fwd_back}}
 #' 
 #' @author Tyler D. Hether
 #' 
 #' @export tetrad_est_fwd_back
 #' 
 #' @examples
-# Example 1: a single tetrad:
+#' # Example 1: a single tetrad:
 #' set.seed(1234567) # For reproducability
 #' l <- 10 # number of loci to simulate
 #' rec <- 0.01 # recombination rate between each snp
@@ -89,7 +176,7 @@ tetrad_est_fwd_back <- function(snp.dat, tetrad.id=1, chr.name="I", p.assign, p.
 
         # 1) establish matrices/vectors:
         emissions <- forward <- backward <- posterior <- matrix(0, ncol=2, nrow=n_snps)
-        scale <- numeric(n_snps)
+        scale <- scaleb <- numeric(n_snps)
         #
         # 2) calculate emission probabilities for all states at all positions
         # data at position i are counts n_ij of allele matching parent j, with n_i = sum(n_ij)
@@ -111,25 +198,27 @@ tetrad_est_fwd_back <- function(snp.dat, tetrad.id=1, chr.name="I", p.assign, p.
         scale[1] <- sum(forward[1,])
         forward[1,] <- forward[1,]/scale[1]         #re-scale forward probabilities by their sum to avoid underflow
         for(i in 2:n_snps){
-            transition_matrix_i <- matrix(c(1-(displace[i-1]*p.trans), displace[i-1]*p.trans, displace[i-1]*p.trans, 1-(displace[i-1]*p.trans)), byrow=TRUE, ncol=2)
-            forward[i,1] <- sum(transition_matrix_i[1,1]*forward[i-1,1], transition_matrix_i[2,1]*forward[i-1,2]) * emissions[i,1]
-            forward[i,2] <- sum(transition_matrix_i[1,2]*forward[i-1,1], transition_matrix_i[2,2]*forward[i-1,2]) * emissions[i,2]
-            # where transition_matrix_i is scaled by the distance between position i-1 and i
+            T <- matrix(c(1-(displace[i-1]*p.trans), displace[i-1]*p.trans, displace[i-1]*p.trans, 1-(displace[i-1]*p.trans)), byrow=TRUE, ncol=2)
+            e <- diag(emissions[i,])
+            forward[i,] <- e %*% T %*% forward[i-1,]
             scale[i] = sum(forward[i,])
             forward[i,] = forward[i,]/scale[i]
         }
 
         # 5) calculate backward probabilities:
-        backward[n_snps,] = 1/scale[n_snps]
+        backward[n_snps,] = c(1,1) #1/scale[n_snps]
         for(i in (n_snps-1):1){
-            transition_matrix_i <- matrix(c(1-(displace[i]*p.trans), displace[i]*p.trans, displace[i]*p.trans, 1-(displace[i]*p.trans)), byrow=TRUE, ncol=2)
-            backward[i,1] <- sum(transition_matrix_i[1,1]*backward[i+1,1], transition_matrix_i[2,1]*backward[i+1,2]) * emissions[i+1,1]
-            backward[i,2] <- sum(transition_matrix_i[1,2]*backward[i+1,1], transition_matrix_i[2,2]*backward[i+1,2]) * emissions[i+1,2]
-            # transition matrix scaled by distance from i to i+1
-            # note the i+1 for the emissions, not i
-            backward[i,] = backward[i,]/scale[i]
+            T <- matrix(c(1-(displace[i]*p.trans), displace[i]*p.trans, displace[i]*p.trans, 1-(displace[i]*p.trans)), byrow=TRUE, ncol=2)
+            e <- diag(emissions[i+1,])
+            backward[i,] <- T %*% e %*% backward[i+1,]
+            # backward[i+1,] %*% T %*% e # old (incorrect) way
+            # Hardcoded check using algorithm on page 60 of Durbin et al. 1998
+            # sum(T[1,1]*emissions[i+1,1]*backward[i+1,1], T[2,1]*emissions[i+1,2]*backward[i+1,2])
+            # sum(T[2,1]*emissions[i+1,1]*backward[i+1,1], T[2,2]*emissions[i+1,2]*backward[i+1,2])
+            scaleb[i] = sum(backward[i,])
+            backward[i,] = backward[i,]/scaleb[i]
         }
-                
+
         # 6) caculate posteriors: 
         for(i in 1:n_snps){
             posterior[i,] <- (forward[i,]*backward[i,])/(forward[i,1]*backward[i,1] + forward[i,2]*backward[i,2])
@@ -228,8 +317,8 @@ est_fwd_back <- function(single.snp.dat, spore_number=1, chr.name="I", p.assign,
     # emissions[i][j] = (n_i choose n_ij) * (1 - eps)^(n_ij) * (eps)^(n_i - n_ij)
     # if no data at SNP i, emissions[i][j] = 1 for all j
     #
-    k0 <- single.snp.dat$p0.assign
-    k1 <- single.snp.dat$p1.assign
+    k0 <- snp.dat[[spore_number]]$p0.assign
+    k1 <- snp.dat[[spore_number]]$p1.assign
     n_i <- k0+k1
     p0 <- choose(n_i,k0)*(p.assign^k0)*((1-p.assign)^(n_i-k0))
     p1 <- choose(n_i,k1)*(p.assign^k1)*((1-p.assign)^(n_i-k1))    
@@ -243,25 +332,27 @@ est_fwd_back <- function(single.snp.dat, spore_number=1, chr.name="I", p.assign,
     scale[1] <- sum(forward[1,])
     forward[1,] <- forward[1,]/scale[1]         #re-scale forward probabilities by their sum to avoid underflow
     for(i in 2:n_snps){
-        transition_matrix_i <- matrix(c(1-(displace[i-1]*p.trans), displace[i-1]*p.trans, displace[i-1]*p.trans, 1-(displace[i-1]*p.trans)), byrow=TRUE, ncol=2)
-        forward[i,1] <- sum(transition_matrix_i[1,1]*forward[i-1,1], transition_matrix_i[2,1]*forward[i-1,2]) * emissions[i,1]
-        forward[i,2] <- sum(transition_matrix_i[1,2]*forward[i-1,1], transition_matrix_i[2,2]*forward[i-1,2]) * emissions[i,2]
-        # where transition_matrix_i is scaled by the distance between position i-1 and i
+        T <- matrix(c(1-(displace[i-1]*p.trans), displace[i-1]*p.trans, displace[i-1]*p.trans, 1-(displace[i-1]*p.trans)), byrow=TRUE, ncol=2)
+        e <- diag(emissions[i,])
+        forward[i,] <- e %*% T %*% forward[i-1,]
         scale[i] = sum(forward[i,])
         forward[i,] = forward[i,]/scale[i]
     }
 
     # 5) calculate backward probabilities:
-    backward[n_snps,] = 1/scale[n_snps]
+    backward[n_snps,] = c(1,1) #1/scale[n_snps]
     for(i in (n_snps-1):1){
-        transition_matrix_i <- matrix(c(1-(displace[i]*p.trans), displace[i]*p.trans, displace[i]*p.trans, 1-(displace[i]*p.trans)), byrow=TRUE, ncol=2)
-        backward[i,1] <- sum(transition_matrix_i[1,1]*backward[i+1,1], transition_matrix_i[2,1]*backward[i+1,2]) * emissions[i+1,1]
-        backward[i,2] <- sum(transition_matrix_i[1,2]*backward[i+1,1], transition_matrix_i[2,2]*backward[i+1,2]) * emissions[i+1,2]
-        # transition matrix scaled by distance from i to i+1
-        # note the i+1 for the emissions, not i
-        backward[i,] = backward[i,]/scale[i]
+        T <- matrix(c(1-(displace[i]*p.trans), displace[i]*p.trans, displace[i]*p.trans, 1-(displace[i]*p.trans)), byrow=TRUE, ncol=2)
+        e <- diag(emissions[i+1,])
+        backward[i,] <- T %*% e %*% backward[i+1,]
+        # backward[i+1,] %*% T %*% e # old (incorrect) way
+        # Hardcoded check using algorithm on page 60 of Durbin et al. 1998
+        # sum(T[1,1]*emissions[i+1,1]*backward[i+1,1], T[2,1]*emissions[i+1,2]*backward[i+1,2])
+        # sum(T[2,1]*emissions[i+1,1]*backward[i+1,1], T[2,2]*emissions[i+1,2]*backward[i+1,2])
+        scaleb[i] = sum(backward[i,])
+        backward[i,] = backward[i,]/scaleb[i]
     }
-                
+
     # 6) caculate posteriors: 
     for(i in 1:n_snps){
         posterior[i,] <- (forward[i,]*backward[i,])/(forward[i,1]*backward[i,1] + forward[i,2]*backward[i,2])
