32d9e3c01b371324ce6c0d2c87907f7ee4e5a2c5
Tyler Hether <tyler.hether@gmail.com>
2015-01-24 22:51:49 -0800

    Major change for input of fb algorithm. It will now take simulated or empirical tetrads and spores.

--- R/forward-backward.R
+++ R/forward-backward.R
@@ -1,5 +1,4 @@
-#' @title Inferring genotypic states using the Forward-Backward algorithm when all spores of a 
-#' tetrad are genotyped
+#' @title Needs updating: Inferring genotypic states using the Forward-Backward algorithm
 #' 
 #' @description Uses the forward-backward algorithm to estimate ancestral genotypes 
 #' along a given chromosome for a given genotyped tetrad or simulated data.
@@ -124,185 +123,65 @@
 #' Drubin, R. S. Eddy, A. Krogh, and G. Mitchison. 1998. Biological Sequence Analysis: Probabilistic Models
 #' of proteins and nucleic acids. Cambridge University Press, Cambridge CB2 8RU, UK.
 #' 
-#' @seealso \code{\link{simulate_coverage}}, \code{\link{est_fwd_back}}
-#' 
-#' @author Tyler D. Hether
-#' 
-#' @export tetrad_est_fwd_back
-#' 
-#' @examples
-#' # Example 1: a single tetrad:
-#' set.seed(1234567) # For reproducability
-#' l <- 10 # number of loci to simulate
-#' rec <- 0.01 # recombination rate between each snp
-#' r <- recombine_index(rep(rec, l-1)) # recombination rate between each snp (vector form)
-#' # probability of correct sequencing assignment (1-sequence error rate) 
-#' # + probabilty of mutation that makes snps identical by state
-#' p_a <- .999 
-#' p <- make_parents(l) # make the parent
-#' recomb_sim <- recombine(parents=p, r.index=r, mu.rate=0, f.cross=.5, f.convert=1, length.conversion=10) # recombine parents
-#' sim_reads <- simulate_coverage(simdata=recomb_sim, p.assign=p_a, coverage=1) # simulate sequencing coverage
-#' # Use the forward-backward algorithm to get the posterior probability of parent '0' ancestry and infer states
-#' fbres <- tetrad_est_fwd_back(snp.dat=sim_reads, tetrad.id=2, chr.name="II", p.assign=p_a, p.trans=rec)
-#' fbres
-#' #
-#' #
-#' # Example 2: 500 simulated tetrads
-#' set.seed(1234567) # For reproducability
-#' l <- 10
-#' rec <- rep(0.01, l-1)
-#' n.tetrads <- 500 # number of spores to simulate
-#' res <- sim_tetrad(n.tetrads=n.tetrads, l=l, rec=rec, p.assign=0.999, 
-#'    mu.rate=0, f.cross=0.8, f.convert=0.9, length.conversion=2, coverage=2.5)
-#' # loop through the results list and estimate parental states for each of the n.tetrads
-#' fbres2 <- lapply(1:length(res), function(Z){
-#'         tetrad_est_fwd_back(snp.dat=res[[Z]], tetrad.id=Z, chr.name="I", p.assign=0.999, p.trans=mean(rec))   
-#'     })
-#' fbres2
-
-tetrad_est_fwd_back <- function(snp.dat, tetrad.id=1, chr.name="I", p.assign, p.trans){
-
-    # Run this entire block of code for each of the four spores:
-    all_spores <- lapply(1:4, function(x, ...){
-        spore_number <- x
-  
-        snp.locations <- snp.dat$snps
-
-        check_est_input(snp.dat, spore_number, chr.name, snp.locations, p.assign, p.trans)
-        # 
-        displace <- snp.locations[-c(1)]-snp.locations[1:(length(snp.locations)-1)]
-          
-        n_snps <- length(snp.locations)
-
-        # 1) establish matrices/vectors:
-        emissions <- forward <- backward <- posterior <- matrix(0, ncol=2, nrow=n_snps)
-        scale <- scaleb <- numeric(n_snps)
-        #
-        # 2) calculate emission probabilities for all states at all positions
-        # data at position i are counts n_ij of allele matching parent j, with n_i = sum(n_ij)
-        # emissions[i][j] = (n_i choose n_ij) * (1 - eps)^(n_ij) * (eps)^(n_i - n_ij)
-        # if no data at SNP i, emissions[i][j] = 1 for all j
-        #
-        k0 <- snp.dat[[spore_number]]$p0.assign
-        k1 <- snp.dat[[spore_number]]$p1.assign
-        n_i <- k0+k1
-        p0 <- choose(n_i,k0)*(p.assign^k0)*((1-p.assign)^(n_i-k0))
-        p1 <- choose(n_i,k1)*(p.assign^k1)*((1-p.assign)^(n_i-k1))    
-        emissions <- t(rbind(p0, p1))
-        #
-        # 3) calculate pi[k] (vector of state probabilities at first position -- here use 1/k for each)
-        #
-        pi_initial <- c(0.5, 0.5)
-        # 4) calculate forward probabilities and scaling factor:
-        forward[1,] <- pi_initial*emissions[1,]
-        scale[1] <- sum(forward[1,])
-        forward[1,] <- forward[1,]/scale[1]         #re-scale forward probabilities by their sum to avoid underflow
-        for(i in 2:n_snps){
-            T <- matrix(c(1-(displace[i-1]*p.trans), displace[i-1]*p.trans, displace[i-1]*p.trans, 1-(displace[i-1]*p.trans)), byrow=TRUE, ncol=2)
-            e <- diag(emissions[i,])
-            forward[i,] <- e %*% T %*% forward[i-1,]
-            scale[i] = sum(forward[i,])
-            forward[i,] = forward[i,]/scale[i]
-        }
-
-        # 5) calculate backward probabilities:
-        backward[n_snps,] = c(1,1) #1/scale[n_snps]
-        for(i in (n_snps-1):1){
-            T <- matrix(c(1-(displace[i]*p.trans), displace[i]*p.trans, displace[i]*p.trans, 1-(displace[i]*p.trans)), byrow=TRUE, ncol=2)
-            e <- diag(emissions[i+1,])
-            backward[i,] <- T %*% e %*% backward[i+1,]
-            # backward[i+1,] %*% T %*% e # old (incorrect) way
-            # Hardcoded check using algorithm on page 60 of Durbin et al. 1998
-            # sum(T[1,1]*emissions[i+1,1]*backward[i+1,1], T[2,1]*emissions[i+1,2]*backward[i+1,2])
-            # sum(T[2,1]*emissions[i+1,1]*backward[i+1,1], T[2,2]*emissions[i+1,2]*backward[i+1,2])
-            scaleb[i] = sum(backward[i,])
-            backward[i,] = backward[i,]/scaleb[i]
-        }
-
-        # 6) caculate posteriors: 
-        for(i in 1:n_snps){
-            posterior[i,] <- (forward[i,]*backward[i,])/(forward[i,1]*backward[i,1] + forward[i,2]*backward[i,2])
-        }
-        # Bug: in rare cases the denominator of the above formula returns 0. This
-        # happens when there is the foward and backward probabilities each return 0 for alternate states.
-        # The result is "NaN" produced as a result. 
-        # The function check_tie (infer-recom.R) will treat these cases as missing data. 
-
-
-        # 7) total likelihood:
-        lnL = sum(log(scale))
-
-        # 8) Call states 0 or 1 based on the posterior probs. If it's a tie, pick one at random:
-        states_inferred <- apply(posterior, 1, function(i){
-            if(check_tie(i)==0){
-                return(which(i==max(i))-1)
-            }
-            if(check_tie(i)==1 | NaN %in% i){
-                # The first is typically true for low coverage data and address bug #6.
-                # The second typically occurs with high coverage data and addresses bug #4.
-                return(sample(c(0,1), 1, prob=c(0.5,0.5)))
-            }
-        })
-
-        out <- list(snp.dat=snp.dat, spore_number=spore_number, tetrad.id=tetrad.id, chr.name=chr.name, 
-                 snp.locations=snp.locations, p.assign=p.assign, p.trans=p.trans,
-                 emissions=emissions, forward=forward, backward=backward, scale=scale, 
-                 posterior=posterior, states_inferred=states_inferred, lnL=lnL)
-
-        # class(out) <- c("list", "forward.backward")
-        return(out)
-    })
-    class(all_spores) <- c("list", "forward.backward")
-    return(all_spores)
-}
-
-#' @title Infer genotypic states using the Forward-Backward algorithm when spores are randomly
-#' sampled en masse
-#'
-#' @description to do 
-#' 
-#' @param to do 
-#' 
-#' @return to do
-#' 
-#' @references Hohenlohe, P.A., S. Bassham, M. Currey, and W.A. Cresko. 2012. Extensive linkage 
-#' disequilibrium and parallel adaptive divergence across threespine stickleback genomes. 
-#' Phil. Trans. R. Soc. B, 395-408, doi: 10.1098/rstb.2011.0245
-#' 
-#' @seealso \code{\link{tetrad_est_fwd_back}}
+#' @seealso \code{\link{simulate_coverage}}
 #' 
 #' @author Tyler D. Hether
 #' 
 #' @export est_fwd_back
 #' 
 #' @examples
+#' # Ex1 (from tetrads)
+#' set.seed(1234567) # For reproducability
+#' l <- 10
+#' rec <- rep(0.01, l-1)
+#' n.tetrads <- 500 # number of spores to simulate
+#' res <- sim_tetrad(n.tetrads=n.tetrads, l=l, rec=rec, p.assign=0.999, 
+#'    mu.rate=0, f.cross=0.8, f.convert=0.9, length.conversion=2, coverage=2.5)
+#' snp.dat <- tetrad_to_df(res)
+#' ddply(snp.dat, .(Tetrad, Spore, Chr), function(x){
+#'     est_fwd_back(snp.dat=x, p.assign=0.999, p.trans=0.01)
+#'     })
+#' #
+#' # Ex 2 (from single spores)
 #' set.seed(1234567) # For reproducability
 #' # simulate a recombination hotspot between the 100th and 101st snp
 #' rec <- c(rep(0.001, 99), 0.1, rep(0.001, 99))
 #' n.spores <- 500 # number of spores to simulate
 #' spores <- sim_en_masse(n.spores=n.spores, l=200, rec=rec, 
-#'  p.assign=.999, mu.rate=0.001, f.cross=0.5, 
+#' p.assign=.999, mu.rate=0.001, f.cross=0.5, 
 #'     f.convert=0.5, length.conversion=10, coverage=1)
-#' 
-#' # Run the fb algorithm to estimate the parental states:
-#' Allspores <- lapply(1:n.spores, function(Z){
-#'         fbres2 <- est_fwd_back(single.snp.dat=spores[[Z]], 
-#'             spore_number=Z, chr.name="I", p.assign=0.999, 
-#'             p.trans=mean(rec))
-#'         return(fbres2)
+#' snp.dat <- en_masse_to_df(spores)
+#' ddply(snp.dat, .(Tetrad, Spore, Chr), function(x){
+#'     est_fwd_back(snp.dat=x, p.assign=0.999, p.trans=0.01)
 #'     })
-#' 
-#' df <- do.call(rbind,lapply(Allspores, function(i){
-#'  return(as.numeric(i$states_inferred))}))
-#' 
-#' plot(apply(t(apply(df, 1, id_hotspots)), 2, sum), 
-#'  type="l", xlab="snp", ylab="Number of recombination events")
+#
+# snp.dat <- read.table("./inst/testdata/exampleEmpiricaldata.txt", header=TRUE)
+# # snp.dat <- dplyr::filter(snp.dat, Tetrad==2, Spore==3, Chr==1)
+# ddply(snp.dat, .(Tetrad, Spore, Chr), function(x){
+#     est_fwd_back(snp.dat=x, p.assign=0.999, p.trans=0.01)
+#     })
 
-est_fwd_back <- function(single.snp.dat, spore_number=1, chr.name="I", p.assign, p.trans){
- 
-    snp.locations <- single.snp.dat$snps
+est_fwd_back <- function(snp.dat, p.assign, p.trans){
+    # colnames(snp.dat) == c("Tetrad", "Spore", "Chr", "Snp", "p0", "p1")
+    if(!inherits(snp.dat, "data.frame")) {
+        if(inherits(snp.dat, "tetrad")) {
+            snp.dat <- tetrad_to_df(snp.dat)
+            }
+        if(inherits(snp.dat, "en.masse")){
+            snp.dat <- en_masse_to_df(snp.dat)
+            }
+    }
+    if(!inherits(snp.dat, "data.frame")){
+        stop("Object snp.dat needs to be of type data.frame 
+        with columns c('Tetrad', 'Spore', 'Chr', 'Snp', 'p0', 'p1')")
+    } else {
+        tetrad.id <- snp.dat[,1]
+        spore_number <- snp.dat[,2]
+        chr.name <- snp.dat[,3]
+        snp.locations <- snp.dat[,4]        
+    }  
 
-    check_est_input_single(single.snp.dat, chr.name, snp.locations, p.assign, p.trans)
+    check_est_input(p.assign, p.trans)
     # 
     displace <- snp.locations[-c(1)]-snp.locations[1:(length(snp.locations)-1)]
           
@@ -310,15 +189,15 @@ est_fwd_back <- function(single.snp.dat, spore_number=1, chr.name="I", p.assign,
 
     # 1) establish matrices/vectors:
     emissions <- forward <- backward <- posterior <- matrix(0, ncol=2, nrow=n_snps)
-    scale <- numeric(n_snps)
+    scale <- scaleb <-  numeric(n_snps)
     #
     # 2) calculate emission probabilities for all states at all positions
     # data at position i are counts n_ij of allele matching parent j, with n_i = sum(n_ij)
     # emissions[i][j] = (n_i choose n_ij) * (1 - eps)^(n_ij) * (eps)^(n_i - n_ij)
     # if no data at SNP i, emissions[i][j] = 1 for all j
     #
-    k0 <- snp.dat[[spore_number]]$p0.assign
-    k1 <- snp.dat[[spore_number]]$p1.assign
+    k0 <- snp.dat[,3]
+    k1 <- snp.dat[,4]
     n_i <- k0+k1
     p0 <- choose(n_i,k0)*(p.assign^k0)*((1-p.assign)^(n_i-k0))
     p1 <- choose(n_i,k1)*(p.assign^k1)*((1-p.assign)^(n_i-k1))    
@@ -378,41 +257,79 @@ est_fwd_back <- function(single.snp.dat, spore_number=1, chr.name="I", p.assign,
         }
     })
 
-    out <- list(single.snp.dat=single.snp.dat, spore_number=spore_number, chr.name=chr.name, 
-             snp.locations=snp.locations, p.assign=p.assign, p.trans=p.trans,
-             emissions=emissions, forward=forward, backward=backward, scale=scale, 
-             posterior=posterior, states_inferred=states_inferred, lnL=lnL)
+    out <- data.frame(Tetrad=tetrad.id, spore_number=spore_number, chr.name=chr.name, 
+             snp=snp.locations, emiss0=emissions[,1], emiss1=emissions[,2],
+             forward0=forward[,1], forward1=forward[,2], backward0=backward[,1],
+             backward1=backward[,2], Fscale=scale, Bscale=scaleb,
+             posterior0=posterior[,1], posterior1=posterior[,2],
+             states_inferred=states_inferred, lnL=rep(lnL, length(n_snps)))
 
-    class(out) <- c("list", "single.forward.backward")
+    class(out) <- c("data.frame")
     return(out)
 }
 
+#' @title Convert from class tetrad to class data.frame
+#' 
+#' @description Converts from simlated data of class tetrad to class data.frame
+#' 
+#' @param class.tetrad a list of class tetrad
+#' 
+#' @return a data frame with 6 columns, c("Tetrad", "Spore", "Chr", "Snp", "p0", "p1")
+#' 
+#' @export tetrad_to_df
+
+tetrad_to_df <- function(class.tetrad){
+    if(!(inherits(class.tetrad, "tetrad"))){
+        stop("Object class.tetrad needs to be of class tetrad")
+    }
+    out <- lapply(1:length(class.tetrad), function(x){
+        n_snps <- length(class.tetrad[[x]]$snps)
+        return(data.frame(Tetrad=rep(rep(x, n_snps),4), 
+            Spore=rep(c(1,2,3,4), each=n_snps), 
+            Chr=rep(rep(1, n_snps),4), 
+            Snp=rep(class.tetrad[[x]]$snps, 4), 
+            p0=c(class.tetrad[[x]][[1]]$p0.assign, class.tetrad[[x]][[2]]$p0.assign,
+            class.tetrad[[x]][[3]]$p0.assign,class.tetrad[[x]][[4]]$p0.assign), 
+            p1=c(class.tetrad[[x]][[1]]$p1.assign, class.tetrad[[x]][[2]]$p1.assign,
+            class.tetrad[[x]][[3]]$p1.assign,class.tetrad[[x]][[4]]$p1.assign)))
+    })
+    out2 <- do.call(rbind, out)
+    return(out2)
+}
+
+#' @title Convert from class en.masse to class data.frame
+#' 
+#' @description Converts from simlated data of class en.masse to class data.frame
+#' 
+#' @param class.en.masse a list of class en.masse
+#' 
+#' @return a data frame with 6 columns, c("Tetrad", "Spore", "Chr", "Snp", "p0", "p1")
+#' 
+#' @export en_masse_to_df
+
+en_masse_to_df <- function(class.en.masse){
+    if(!(inherits(class.en.masse, "en.masse"))){
+        stop("Object class.en.masse needs to be of class en.masse")
+    }
+    out <- lapply(1:length(class.en.masse), function(x){
+        n_snps <- length(class.en.masse[[x]]$snps)
+        return(data.frame(Tetrad=rep(x, n_snps), 
+            Spore=rep(x, n_snps), 
+            Chr=rep(1, n_snps), 
+            Snp=class.en.masse[[x]]$snps, 
+            p0=class.en.masse[[x]]$p0.assign, 
+            p1=class.en.masse[[x]]$p1.assign))
+    })
+    out2 <- do.call(rbind, out)
+    return(out2)
+}
+
+
 # Minor functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-check_est_input <- function(snp.dat, spore_number, chr.name, snp.locations, p.assign, p.trans, ...){
-    if (!inherits(snp.dat, "snp.recom") & !inherits(snp.dat, "individual.tetrad"))
-        stop("Object must be of class 'snp.recom' or 'individual.tetrad'")
+check_est_input <- function(p.assign, p.trans, ...){
     check_values(p.assign, 0, 1)
     check_values(p.trans, 0, 1)
-    if(length(snp.dat[[1]]$p0.assign)!=length(unique(snp.locations))){
-        stop("Each snp needs 1 unique numeric id.")
-    }
-    if(!spore_number %in% c(1:4)){
-        stop("spore_number must be an the integer 1, 2, 3, or 4")
-    }
 }
-
-check_est_input_single <- function(single.snp.dat, chr.name, snp.locations, p.assign, p.trans){
-    if (!inherits(single.snp.dat, "single.spore"))
-        stop("Object must be of class 'single.spore'")
-    check_values(p.assign, 0, 1)
-    check_values(p.trans, 0, 1)
-    if(length(single.snp.dat$p0.assign)!=length(unique(snp.locations))){
-        stop("Each snp needs 1 unique numeric id.")
-    }
-}
-
-
-
 
 #' @method print estimate_anc_fwd_back
 #' @export 
