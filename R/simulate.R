86212d215eb1e11800b2b3518b7c1422e08fcb1a
Tyler Hether <tyler.hether@gmail.com>
2015-05-28 10:25:23 -0700

    Fixed error in en_masse simulations.

--- R/simulate.R
+++ R/simulate.R
@@ -383,17 +383,16 @@ sim_tetrad <- function(n.tetrads, l, scale, snps, p.assign, mu.rate, f.cross, f.
 #' # simulate a recombination hotspot between the 100th and 101st snp
 #' l=100; scale = 0.01; snps=1:l
 #' n.spores <- 500 # number of spores to simulate
-#' spores <- sim_en_masse(n.spores=n.spores, l=l, scale=scale, snps=snps, 
-#' p.assign=.999, mu.rate=0.001, f.cross=0.5, 
-#'     f.convert=0.5, length.conversion=10, coverage=1)
+spores <- sim_en_masse(n.spores=n.spores, scale=scale, snps=snps, 
+p.assign=.999, mu.rate=0.001, f.cross=0.5, 
+    f.convert=0.5, length.conversion=10, coverage=1)
 
-sim_en_masse <- function(n.spores, l, scale, snps, p.assign, mu.rate, f.cross, f.convert, 
-        length.conversion, coverage){
+sim_en_masse <- function(n.spores, scale, snps, p.assign, mu.rate, f.cross, f.convert, length.conversion, coverage){
 
     out <- lapply(1:n.spores, function(Z, ...){
 
         r <- recombine_index(scale, snps)
-        p <- make_parents(l)
+        p <- make_parents(snps)
         recomb_sim <- recombine(parents=p, r.index=r, mu.rate=mu.rate, f.cross=f.cross, 
                 f.convert=f.convert, length.conversion=length.conversion)
         sim_reads <- simulate_coverage(simdata=recomb_sim, p.assign=p.assign, coverage=coverage)
